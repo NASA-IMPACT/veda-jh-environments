2b184e57b01b1d16f8637eff3e4fa21f8cab8577
ranchodeluxe <greg.corradini@gmail.com>
2023-05-02 08:24:40 -0700

    tf changes part 2

--- terraform/github-actions-ecr-deploy-user.tf
+++ terraform/github-actions-ecr-deploy-user.tf
@@ -1,31 +1,30 @@
 ########################################
 # Github Deploy User
 ########################################
-
 resource "aws_iam_user" "deploy_user" {
   name = "${var.project_name}-${var.env}-deploy-user"
   path = "/"
   tags = var.tags
 }
 
-// NOTE: we need to have extra policies added to our
-// deploy user for Github AWS Actions to work
-resource "aws_iam_user_policy" "deploy" {
-  name   = "${var.project_name}_deploy_extended"
-  user   = aws_iam_user.deploy_user.name
-  policy = data.aws_iam_policy_document.extended_deploy.json
-}
-
-data "aws_iam_policy_document" "extended_deploy" {
+data "aws_iam_policy_document" "deploy" {
   statement {
+    # TODO: figure out which actions to limit later once we've plumbed out all the CI/CD github actions calls from awscli
     actions = [
       "iam:PassRole",
-      "ecr:InitiateLayerUpload",
+      "sts:GetServiceBearerToken",
+      "ecr-public:*",
+      "ecr:*",
     ]
 
     resources = [
-      module.ecr_registry_base.registry_arn,
-      module.ecr_registry_custom.registry_arn,
+      "*"
     ]
   }
+}
+
+resource "aws_iam_user_policy" "deploy" {
+  name   = "${var.project_name}-${var.env}-deploy-policy"
+  user   = aws_iam_user.deploy_user.name
+  policy = data.aws_iam_policy_document.deploy.json
 }
