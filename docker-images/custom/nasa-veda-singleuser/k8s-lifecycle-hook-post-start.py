7f296ef24425854b10db4d1081e88ec47187326f
ranchodeluxe <greg.corradini@gmail.com>
2023-09-08 08:33:18 -0700

    add post-start script

---
+++ docker-images/custom/nasa-veda-singleuser/k8s-lifecycle-hook-post-start.py
@@ -0,0 +1,15 @@
+import subprocess
+
+try:
+    result = subprocess.run(
+        ["/srv/conda/envs/notebook/bin/gitpuller", "https://github.com/NASA-IMPACT/veda-docs/", "main", "/home/jovyan/veda-doc-examples"],
+        check=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True  # Capture output as a string instead of bytes
+    )
+    print(f"Command stdout:\n{result.stdout}")
+    print(f"Command stderr:\n{result.stderr}")
+except subprocess.CalledProcessError as e:
+    print(f"Command failed with error {e.returncode}, stderr:\n{e.stderr}")
+    raise
