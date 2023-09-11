4608f9a840fa98bfd227449ebfb428d5b15e0b5d
ranchodeluxe <greg.corradini@gmail.com>
2023-09-11 12:02:17 -0700

    add path to init script

--- docker-images/base/nasa-veda-singleuser-init/k8s-lifecycle-hook-post-start.py
+++ docker-images/base/nasa-veda-singleuser-init/k8s-lifecycle-hook-post-start.py
@@ -1,15 +1,26 @@
 import subprocess
+import argparse
 
-try:
-    result = subprocess.run(
-        ["gitpuller", "https://github.com/NASA-IMPACT/veda-docs/", "main", "/home/jovyan/veda-doc-examples"],
-        check=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        text=True  # Capture output as a string instead of bytes
-    )
-    print(f"Command stdout:\n{result.stdout}")
-    print(f"Command stderr:\n{result.stderr}")
-except subprocess.CalledProcessError as e:
-    print(f"Command failed with error {e.returncode}, stderr:\n{e.stderr}")
-    raise
+
+def clone_veda_docs(target_home_path):
+    try:
+        target_path = os.path.join(target_home_path, "veda-docs-examples")
+        result = subprocess.run(
+            ["gitpuller", "https://github.com/NASA-IMPACT/veda-docs/", "main", target_path],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True  # Capture output as a string instead of bytes
+        )
+        print(f"Command stdout:\n{result.stdout}")
+        print(f"Command stderr:\n{result.stderr}")
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed with error {e.returncode}, stderr:\n{e.stderr}")
+        raise
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("target", required=True, help="The target home path which should exist.")
+    args = parser.parse_args()
+    clone_veda_docs(parser.target)
