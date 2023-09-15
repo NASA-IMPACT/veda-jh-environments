3f78ab26d79e2c60c482b9e6f45d01c39919a377
ranchodeluxe <greg.corradini@gmail.com>
2023-09-15 07:08:46 -0700

    no more /veda-docs-examples

--- docker-images/base/nasa-veda-singleuser-init/k8s-init-container-nb-docs.py
+++ docker-images/base/nasa-veda-singleuser-init/k8s-init-container-nb-docs.py
@@ -6,7 +6,7 @@ import logging
 
 logger = logging.getLogger("k8s-init-veda-docs")
 logging.basicConfig(
-    filename='/home/jovyan/k8s-init-veda-doc-examples.log',
+    filename='/home/jovyan/_init_.log',
     level=logging.DEBUG,
     format='%(asctime)s [%(levelname)s] - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
@@ -14,33 +14,30 @@ logging.basicConfig(
 
 
 def clone_veda_docs(target_home_path):
-    try:
-        target_path = os.path.join(target_home_path, "veda-docs-examples")
-        cmd = ["gitpuller", "https://github.com/NASA-IMPACT/veda-docs/", "main", target_path]
-        logger.debug(f"[ EXECUTING ]: {cmd}")
-        result = subprocess.run(
-            cmd,
-            check=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True  # Capture output as a string instead of bytes
-        )
-        logger.debug(f"gitpuller stdout:\n{result.stdout}")
-        logger.error(f"gitpuller stderr:\n{result.stderr}")
+    target_path = os.path.join(target_home_path, "veda-docs")
+    cmd = ["gitpuller", "https://github.com/NASA-IMPACT/veda-docs/", "main", target_path]
+    logger.debug(f"[ EXECUTING ]: {cmd}")
+    result = subprocess.run(
+        cmd,
+        check=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True  # Capture output as a string instead of bytes
+    )
+    logger.debug(f"gitpuller stdout:\n{result.stdout}")
+    logger.error(f"gitpuller stderr:\n{result.stderr}")
 
-        cmd = ["chown", "-R", "1000:1000", target_path],
-        logger.debug(f"[ EXECUTING ]: {cmd}")
-        result = subprocess.run(
-            cmd,
-            check=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True
-        )
-        logger.debug(f"chown stdout:\n{result.stdout}")
-        logger.error(f"chown stderr:\n{result.stderr}")
-    except subprocess.CalledProcessError as e:
-        logger.error(f"Command failed with error {e.returncode}, stderr:\n{e.stderr}")
+    cmd = ["chown", "-R", "1000:1000", target_path],
+    logger.debug(f"[ EXECUTING ]: {cmd}")
+    result = subprocess.run(
+        cmd,
+        check=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True
+    )
+    logger.debug(f"chown stdout:\n{result.stdout}")
+    logger.error(f"chown stderr:\n{result.stderr}")
 
 
 if __name__ == '__main__':
@@ -49,6 +46,8 @@ if __name__ == '__main__':
         parser.add_argument("target", help="The target home path which should exist.")
         args = parser.parse_args()
         clone_veda_docs(args.target)
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Command failed with error {e.returncode}, stderr:\n{e.stderr}")
     except Exception as e:
         logger.error(f"Command failed with error {e.returncode}, stderr:\n{e.stderr}")
     finally:
